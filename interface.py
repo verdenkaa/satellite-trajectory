09726f7a41d4651309723d92e7d2c52404b5405d
verdenkaa <nikitapahomov97@gmail.com>
2022-12-14 22:17:42 +0300

    need only text

--- interface.py
+++ interface.py
@@ -13,31 +13,46 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 class Menu(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main2.ui', self)
+        uic.loadUi('main.ui', self)
         self.start.clicked.connect(self.count)
 
     def count(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
+        lo = self.longtitude.value()
+        la = self.latitude.value()
+        hight = self.hight.value()
+        speed = self.speed.value()
+        polygons = self.polygons.value()
+        space = self.space.isChecked()
+
+        fig = plt.figure(figsize=(10, 7))
+        ax = fig.add_subplot(121, projection='3d')
+        ax2 = fig.add_subplot(122)
+        ax2.legend()
         ax.set_box_aspect((1, 1, 1))
+        ax.margins(8000, 8000, 8000)
+        ax.autoscale(enable=False, tight=True)
 
-        #ax.patch.set_facecolor('black')
+        if space:
+            ax.patch.set_facecolor('black')
+            ax.set_axis_off()
 
-        #plt.axis('off')
-        #plt.grid(b=None)
 
         earth = Earth()
-        latitude, longitude = 0, 0
-        satellite = Satellite(latitude, longitude)
+        satellite = Satellite(lo, la)
 
-        earth.create()
-        satellite.create()
+        ax = earth.create(ax, polygons)
+        ax, ax2 = satellite.create(ax, ax2, lo, la, hight, speed)
 
         plt.show()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     form = Menu()
     form.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
--- main.py
+++ main.py
@@ -11,8 +11,9 @@ def rotation(x, y, a):
     return (x2, y2)
 
 def state(s: np.ndarray, t: float) -> np.ndarray:
-    M_1 = earth.mass
-    M_2 = satellite.mass
+    M_1 = 5 * (10 ** 16)
+    M_2 = 100
+    G = 6.6743015 * (10 ** (-11))
     x, y, z, vx, vy, vz = s
     r = np.array([0 - x, 0 - y, 0 - z])
     mr = np.linalg.norm(r) ** 3
@@ -24,44 +25,33 @@ def state(s: np.ndarray, t: float) -> np.ndarray:
 
 class Earth:
     def __init__(self, poligons=200):
-        self.poligons = 100
         self.R = 6371
         self.R_atm = 6489
         self.mass = 5 * (10 ** 16)
         self.g = 9.8
 
-    def create(self):
-        u = np.linspace(-np.pi, np.pi, self.poligons)
-        v = np.linspace(0, np.pi, self.poligons)
+
+    def create(self, ax, polygons):
+        polygons *= 100
+        u = np.linspace(-np.pi, np.pi, polygons)
+        v = np.linspace(0, np.pi, polygons)
 
         x = self.R * np.outer(np.cos(u), np.sin(v))
         y = self.R * np.outer(np.sin(u), np.sin(v))
         z = self.R * np.outer(np.ones(np.size(u)), np.cos(v))
 
         im = PIL.Image.open('min_earth.png')
-        im = np.array(im.resize([self.poligons, self.poligons])) / 255
+        im = np.array(im.resize([polygons, polygons])) / 255
 
-        #ax.scatter(0, 0, 0, color="blue")
         ax.plot_surface(x, y, z, rstride=4, cstride=4, facecolors=im, antialiased=True, shade=False)
-        #ax.scatter(15000, 15000, 15000, alpha=0)
-        #ax.scatter(-15000, 15000, 15000, alpha=0)
-        #ax.scatter(15000, -15000, 15000, alpha=0)
-        #ax.scatter(15000, 15000, -15000, alpha=0)
+
+        return ax
 
 
 
 
 class Satellite:
-    def __init__(self, latitude, longitude):
-        self.hight = 500
-
-        x = earth.R + self.hight
-        y = 0
-        z = 0
-
-        self.x, self.z = rotation(x, z, latitude)
-        self.x, self.y = rotation(self.x, y, longitude)
-        self.x, self.y, self.z = round(self.x), round(self.y), round(self.z)
+    def __init__(self, longitude, latitude):
 
         self.velocity = 8000
         self.mass = 100
@@ -74,25 +64,28 @@ class Satellite:
 
 
 
-    def create(self):
-        #ax.scatter(self.x, self.y, self.z, color="red")
+    def create(self, ax, ax2, longitude, latitude, hight, speed):
+
+        x = 6371 + hight
+        y = 0
+        z = 0
+
+        self.x, self.z = rotation(x, z, longitude)
+        self.x, self.y = rotation(self.x, y, latitude)
+        self.x, self.y, self.z = round(self.x), round(self.y), round(self.z)
+        ax.scatter(self.x, self.y, self.z, color="black")
+
+        G = 6.6743015 * (10 ** (-11))
+
         ts = np.linspace(0, 10000, 1000)
         ts2 = np.linspace(0, 30000, 1000)
-        x, z = rotation(self.x, self.z, -latitude)
-        x, y = rotation(x, self.y, -longitude)
-        state0 = np.array([x, y, z, self.vx, self.vy, self.vz])
+        z = 0
+        x, y = rotation(x, y, latitude)
+
+        state0 = np.array([x, y, z, 0, 0, speed])
 
         sol = odeint(state, state0, ts)
 
-        '''sol2 = []
-        for i in range(len(np.array(sol))):
-            x2, y2, z2 = sol[i][:3]
-            _x2, z2 = rotation(x2, z2, latitude)
-            x2, y2 = rotation(x2, y2, longitude)
-
-            sol2.append([x2, y2, z2])
-
-        sol = np.array(sol2)'''
 
         xyz = min(sol, key=lambda x: math.sqrt((x[0] - self.x) ** 2 + (x[1] - self.y) ** 2 + (x[2] - self.z) ** 2))
 
@@ -119,12 +112,6 @@ class Satellite:
 
         print("Out", outgo)
 
-
-        trajectory = [i[0:3] for i in sol]
-        velocity = [i[3:6] for i in sol]
-
-        trajectory_corrected = np.zeros(np.shape(trajectory))
-
         kinetic_enegry = []
         potential_enegry = []
         total_energy = []
@@ -132,15 +119,15 @@ class Satellite:
         for i in sol:
             v = math.sqrt(i[3] ** 2 + i[4] ** 2 + i[5] ** 2)
             h = math.sqrt(i[0] ** 2 + i[1] ** 2 + i[2] ** 2)
-            kinetic_enegry.append(0.5 * satellite.mass * (v ** 2))
-            potential_enegry.append(-1 * G * earth.mass * satellite.mass / h)
+            kinetic_enegry.append(0.5 * self.mass * (v ** 2))
+            potential_enegry.append(-1 * G * 5 * (10 ** 16) * self.mass / h)
             total_energy.append(kinetic_enegry[-1] + potential_enegry[-1])
 
 
-        state0 = np.array([x, y, z, self.vx, self.vy, self.vz + 3])
+        state0 = np.array([x, y, z, 0, 0, speed + 3])
 
         sol = odeint(state, state0, ts2)
-        ax.plot(sol[:, 0], sol[:, 1], sol[:, 2], 'g', label='Trajectory', linewidth=2.0, color="black")
+        ax.plot(sol[:, 0], sol[:, 1], sol[:, 2], 'g', label='Trajectory', linewidth=2.0, color="orange")
 
 
 
@@ -158,29 +145,4 @@ class Satellite:
         ax2.set_xlabel('Time')
         ax2.set_ylabel('Joule')
 
-fig = plt.figure(figsize=(10, 7))
-ax = fig.add_subplot(121, projection='3d')
-ax2 = fig.add_subplot(122)
-ax2.legend()
-ax.set_box_aspect((1, 1, 1))
-ax.margins(8000, 8000, 8000)
-ax.autoscale(enable=False, tight=True)
-
-#ax2.autoscale(enable=True, tight=False)
-
-#ax.patch.set_facecolor('black')
-
-#plt.axis('off')
-#plt.grid(b="visible")
-
-
-G = 6.6743015 * (10**(-11))
-
-earth = Earth()
-latitude, longitude = 90, 180
-satellite = Satellite(latitude, longitude)
-
-earth.create()
-satellite.create()
-
-plt.show()
+        return ax, ax2
